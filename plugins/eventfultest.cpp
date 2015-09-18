9d689512d95c6e8b29ace2caf6cfed6f40d249bc
Pheosics <milesengel@gmail.com>
2015-09-18 16:38:54 -0400

    Not Sure

--- plugins/eventfultest.cpp
+++ plugins/eventfultest.cpp
@@ -1,32 +1,34 @@
 #include "Core.h"
-#include "Error.h"
-#include "Console.h"
-#include "Export.h"
-#include "LuaTools.h"
-#include "MiscUtils.h"
-#include "PluginManager.h"
-#include "VTableInterpose.h"
+#include <Console.h>
+#include <Export.h>
+#include <PluginManager.h>
+#include <string.h>
 
-#include "df/building.h"
+#include <VTableInterpose.h>
+
 #include "df/building_workshopst.h"
-#include "df/construction.h"
+
+#include "df/unit.h"
+#include "df/unit_inventory_item.h"
 #include "df/item.h"
 #include "df/item_actual.h"
-#include "df/job.h"
-#include "df/proj_itemst.h"
-#include "df/proj_unitst.h"
+#include "df/unit_wound.h"
+#include "df/world.h"
 #include "df/reaction.h"
 #include "df/reaction_reagent_itemst.h"
 #include "df/reaction_product_itemst.h"
-#include "df/unit.h"
-#include "df/unit_inventory_item.h"
-#include "df/unit_wound.h"
-#include "df/world.h"
+
+#include "df/proj_itemst.h"
+#include "df/proj_unitst.h"
+
+#include "MiscUtils.h"
+#include "LuaTools.h"
 
 #include "modules/EventManager.h"
 
-#include <string.h>
-#include <stdexcept>
+#include "df/job.h"
+#include "df/building.h"
+#include "df/construction.h"
 
 using std::vector;
 using std::string;
@@ -34,12 +36,13 @@ using std::stack;
 using namespace DFHack;
 using namespace df::enums;
 
-DFHACK_PLUGIN("eventful");
-REQUIRE_GLOBAL(gps);
-REQUIRE_GLOBAL(world);
-REQUIRE_GLOBAL(ui);
+using df::global::gps;
+using df::global::world;
+using df::global::ui;
 
 typedef df::reaction_product_itemst item_product;
+
+DFHACK_PLUGIN("eventful");
 
 struct ReagentSource {
     int idx;
@@ -94,7 +97,7 @@ static bool is_lua_hook(const std::string &name)
 static void handle_fillsidebar(color_ostream &out,df::building_workshopst*,bool *call_native){};
 static void handle_postfillsidebar(color_ostream &out,df::building_workshopst*){};
 
-static void handle_reaction_done(color_ostream &out,df::reaction*, df::reaction_product_itemst*, df::unit *unit, std::vector<df::item*> *in_items,std::vector<df::reaction_reagent*> *in_reag
+static void handle_reaction_done(color_ostream &out,df::reaction*, df::unit *unit, std::vector<df::item*> *in_items,std::vector<df::reaction_reagent*> *in_reag
     , std::vector<df::item*> *out_items,bool *call_native){};
 static void handle_contaminate_wound(color_ostream &out,df::item_actual*,df::unit* unit, df::unit_wound* wound, uint8_t a1, int16_t a2){};
 static void handle_projitem_ci(color_ostream &out,df::proj_itemst*,bool){};
@@ -106,7 +109,7 @@ static void handle_syndrome_test(color_ostream &out,df::creature_interaction_eff
 DEFINE_LUA_EVENT_2(onWorkshopFillSidebarMenu, handle_fillsidebar, df::building_workshopst*,bool* );
 DEFINE_LUA_EVENT_1(postWorkshopFillSidebarMenu, handle_postfillsidebar, df::building_workshopst*);
 
-DEFINE_LUA_EVENT_7(onReactionComplete, handle_reaction_done,df::reaction*, df::reaction_product_itemst*, df::unit *, std::vector<df::item*> *,std::vector<df::reaction_reagent*> *,std::vector<df::item*> *,bool *);
+DEFINE_LUA_EVENT_6(onReactionComplete, handle_reaction_done,df::reaction*, df::unit *, std::vector<df::item*> *,std::vector<df::reaction_reagent*> *,std::vector<df::item*> *,bool *);
 DEFINE_LUA_EVENT_5(onItemContaminateWound, handle_contaminate_wound, df::item_actual*,df::unit* , df::unit_wound* , uint8_t , int16_t );
 //projectiles
 DEFINE_LUA_EVENT_2(onProjItemCheckImpact, handle_projitem_ci, df::proj_itemst*,bool );
@@ -122,10 +125,6 @@ static void handle_job_complete(color_ostream &out,df::job*){};
 static void handle_constructions(color_ostream &out,df::construction*){};
 static void handle_syndrome(color_ostream &out,int32_t,int32_t){};
 static void handle_inventory_change(color_ostream& out,int32_t,int32_t,df::unit_inventory_item*,df::unit_inventory_item*){};
-static void handle_report(color_ostream& out,int32_t){};
-static void handle_unitAttack(color_ostream& out,int32_t,int32_t,int32_t){};
-static void handle_unload(color_ostream& out){};
-static void handle_interaction(color_ostream& out, std::string, std::string, int32_t, int32_t, int32_t, int32_t){};
 DEFINE_LUA_EVENT_1(onBuildingCreatedDestroyed, handle_int32t, int32_t);
 DEFINE_LUA_EVENT_1(onJobInitiated,handle_job_init,df::job*);
 DEFINE_LUA_EVENT_1(onJobCompleted,handle_job_complete,df::job*);
@@ -135,10 +134,6 @@ DEFINE_LUA_EVENT_1(onConstructionCreatedDestroyed, handle_constructions, df::con
 DEFINE_LUA_EVENT_2(onSyndrome, handle_syndrome, int32_t,int32_t);
 DEFINE_LUA_EVENT_1(onInvasion,handle_int32t,int32_t);
 DEFINE_LUA_EVENT_4(onInventoryChange,handle_inventory_change,int32_t,int32_t,df::unit_inventory_item*,df::unit_inventory_item*);
-DEFINE_LUA_EVENT_1(onReport,handle_report,int32_t);
-DEFINE_LUA_EVENT_3(onUnitAttack,handle_unitAttack,int32_t,int32_t,int32_t);
-DEFINE_LUA_EVENT_0(onUnload,handle_unload);
-DEFINE_LUA_EVENT_6(onInteraction,handle_interaction, std::string, std::string, int32_t, int32_t, int32_t, int32_t);
 DFHACK_PLUGIN_LUA_EVENTS {
     DFHACK_LUA_EVENT(onWorkshopFillSidebarMenu),
     DFHACK_LUA_EVENT(postWorkshopFillSidebarMenu),
@@ -159,10 +154,6 @@ DFHACK_PLUGIN_LUA_EVENTS {
     DFHACK_LUA_EVENT(onSyndrome),
     DFHACK_LUA_EVENT(onInvasion),
     DFHACK_LUA_EVENT(onInventoryChange),
-    DFHACK_LUA_EVENT(onReport),
-    DFHACK_LUA_EVENT(onUnitAttack),
-    DFHACK_LUA_EVENT(onUnload),
-    DFHACK_LUA_EVENT(onInteraction),
     DFHACK_LUA_END
 };
 
@@ -223,20 +214,6 @@ static void ev_mng_inventory(color_ostream& out, void* ptr)
     }
     onInventoryChange(out,unitId,itemId,item_old,item_new);
 }
-static void ev_mng_report(color_ostream& out, void* ptr) {
-    onReport(out,(int32_t)ptr);
-}
-static void ev_mng_unitAttack(color_ostream& out, void* ptr) {
-    EventManager::UnitAttackData* data = (EventManager::UnitAttackData*)ptr;
-    onUnitAttack(out,data->attacker,data->defender,data->wound);
-}
-static void ev_mng_unload(color_ostream& out, void* ptr) {
-    onUnload(out);
-}
-static void ev_mng_interaction(color_ostream& out, void* ptr) {
-    EventManager::InteractionData* data = (EventManager::InteractionData*)ptr;
-    onInteraction(out, data->attackVerb, data->defendVerb, data->attacker, data->defender, data->attackReport, data->defendReport);
-}
 std::vector<int> enabledEventManagerEvents(EventManager::EventType::EVENT_MAX,-1);
 typedef void (*handler_t) (color_ostream&,void*);
 static const handler_t eventHandlers[] = {
@@ -250,17 +227,13 @@ static const handler_t eventHandlers[] = {
  ev_mng_syndrome,
  ev_mng_invasion,
  ev_mng_inventory,
- ev_mng_report,
- ev_mng_unitAttack,
- ev_mng_unload,
- ev_mng_interaction,
 };
 static void enableEvent(int evType,int freq)
 {
     if (freq < 0)
         return;
-    CHECK_INVALID_ARGUMENT(evType >= 0 && evType < EventManager::EventType::EVENT_MAX &&
-                           evType != EventManager::EventType::TICK);
+    if (evType < 0 || evType >= EventManager::EventType::EVENT_MAX || evType == EventManager::EventType::TICK)
+        throw std::runtime_error("invalid event type to enable");
     EventManager::EventHandler::callback_t fun_ptr = eventHandlers[evType];
     EventManager::EventType::EventType typeToEnable=static_cast<EventManager::EventType::EventType>(evType);
 
@@ -318,25 +291,18 @@ struct product_hook : item_product {
          int32_t quantity, df::job_skill skill,
          df::historical_entity *entity, df::world_site *site)
     ) {
-        color_ostream_proxy out(Core::getInstance().getConsole());
-        auto product = products[this];
-        if ( !product ) {
-            INTERPOSE_NEXT(produce)(unit, out_items, in_reag, in_items, quantity, skill, entity, site);
-            return;
+        if (auto product = products[this])
+        {
+            df::reaction* this_reaction=product->react;
+            CoreSuspendClaimer suspend;
+            color_ostream_proxy out(Core::getInstance().getConsole());
+            bool call_native=true;
+            onReactionComplete(out,this_reaction,unit,in_items,in_reag,out_items,&call_native);
+            if(!call_native)
+                return;
         }
-        df::reaction* this_reaction=product->react;
-        CoreSuspendClaimer suspend;
-        bool call_native=true;
-        onReactionComplete(out,this_reaction,(df::reaction_product_itemst*)this,unit,in_items,in_reag,out_items,&call_native);
-        if(!call_native)
-            return;
 
-        size_t out_item_count = out_items->size();
         INTERPOSE_NEXT(produce)(unit, out_items, in_reag, in_items, quantity, skill, entity, site);
-        if ( out_items->size() == out_item_count )
-            return;
-        //if it produced something, call the scripts
-        onReactionComplete(out,this_reaction,(df::reaction_product_itemst*)this,unit,in_items,in_reag,out_items,NULL);
     }
 };
 
@@ -418,8 +384,8 @@ static bool find_reactions(color_ostream &out)
 
     for (size_t i = 0; i < rlist.size(); i++)
     {
-        //if (!is_lua_hook(rlist[i]->code))
-        //    continue;
+        if (!is_lua_hook(rlist[i]->code))
+            continue;
         reactions[rlist[i]->code].react = rlist[i];
     }
 
@@ -437,7 +403,7 @@ static bool find_reactions(color_ostream &out)
             parse_product(out, out_prod.back(), it->second.react, itprod);
         }
 
-        for (size_t i = 0; i < out_prod.size(); i++)
+        for (size_t i = 0; i < prod.size(); i++)
         {
             if (out_prod[i].isValid())
                 products[out_prod[i].product] = &out_prod[i];
@@ -461,12 +427,12 @@ static void world_specific_hooks(color_ostream &out,bool enable)
 {
     if(enable && find_reactions(out))
     {
-        //out.print("Detected reaction hooks - enabling plugin.\n");
+        out.print("Detected reaction hooks - enabling plugin.\n");
         INTERPOSE_HOOK(product_hook, produce).apply(true);
     }
     else
     {
-        INTERPOSE_HOOK(product_hook, produce).apply(false);
+       INTERPOSE_HOOK(product_hook, produce).apply(false);
         reactions.clear();
         products.clear();
     }
@@ -484,7 +450,7 @@ DFhackCExport command_result plugin_onstatechange(color_ostream &out, state_chan
         break;
     case SC_WORLD_UNLOADED:
         world_specific_hooks(out,false);
-
+        
         break;
     default:
         break;
